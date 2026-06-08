86996ddbaa44d614db00fee4661a3d25bb8843b3
muruga3393-dev <muruga3393@gmail.com>
2026-06-08 19:44:22 +0530

    script files are deleted

--- for_loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Sample For loop script
-
-for i in red yellow blue green
-do
-	echo $i
-done
-
-
--- messages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Script to live the /var/log/message for the error/shutdown
-
-tail -f /var/log/messages | while read output
-do
-	echo $output | egrep -i "error|warning|shutdown|reboot|su|failed" >> /tmp/messages
-done
-
